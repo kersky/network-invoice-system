45dc92ed6bb471d4bb556b5d98fbab8f8440c003
kersky <kersky@163.com>
2012-09-15 18:19:25 +0800

    SplashActivity

---
+++ src/me/kersky/netwrokinvoicesystem/SplashActivity.java
@@ -0,0 +1,11 @@
+package me.kersky.netwrokinvoicesystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+@Override
+public void onCreate(Bundle savedInstance){
+	
+}
+}
