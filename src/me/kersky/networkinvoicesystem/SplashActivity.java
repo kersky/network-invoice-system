95db7ff2ee294c9349eb0c4363cd05658d82dbb6
kersky <kersky@163.com>
2012-09-15 19:26:52 +0800

    splash emulator ok

---
+++ src/me/kersky/networkinvoicesystem/SplashActivity.java
@@ -0,0 +1,12 @@
+package me.kersky.networkinvoicesystem;
+import me.kersky.networkinvoicesystem.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+@Override
+public void onCreate(Bundle savedInstanceState){
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.splash);
+}
+}
